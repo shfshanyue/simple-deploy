cde5540d8c3dcb23dbf682f6b1f07f7333498621
shfshanyue <xianger94@gmail.com>
2022-02-16 20:02:02 +0800

    chore: server-fs.js for createReadStream

--- server-fs.js
+++ server-fs.js
@@ -4,6 +4,12 @@ const fs = require('node:fs')
 const html = fs.readFileSync('./index.html')
 
 const server = http.createServer((req, res) => res.end(html))
+
+// 实际上，此处使用 Stream 处理为更好的方法，但此时需要额外处理下 Content-Length。因为是示例代码，所以不做强求。
+// const server = http.createServer((req, res) => {
+//   fs.createReadStream('./index.html').pipe(res)
+// })
+
 server.listen(3000, () => {
   console.log('Listening 3000')
 })
